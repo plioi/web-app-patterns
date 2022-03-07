98eb59cfed4529f93c9cb02ffdebb2eed5a4ca52
Patrick Lioi <patrick.lioi@gmail.com>
2022-03-07 17:38:25 -0600

    The database-migrations folder includes scripts to establish the database.

---
+++ database-migrations/up/2018-05-28-1456.Insert_Sample_Contacts.ENV.DEV.sql
@@ -0,0 +1,23 @@
+﻿INSERT INTO [dbo].[Contact] ([Email], [Name], [PhoneNumber]) VALUES (
+    'abe.doe@example.com',
+    'Abe Doe',
+    '555-123-0001'
+);
+
+INSERT INTO [dbo].[Contact] ([Email], [Name], [PhoneNumber]) VALUES (
+    'beth.doe@example.com',
+    'Beth Doe',
+    '555-123-0002'
+);
+
+INSERT INTO [dbo].[Contact] ([Email], [Name], [PhoneNumber]) VALUES (
+    'cathy.doe@example.com',
+    'Cathy Doe',
+    '555-123-0003'
+);
+
+INSERT INTO [dbo].[Contact] ([Email], [Name], [PhoneNumber]) VALUES (
+    'don.doe@example.com',
+    'Don Doe',
+    '555-123-0004'
+);
