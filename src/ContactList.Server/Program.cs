4980d857ef45235c52750d7ddd79e63062623e26
Patrick Lioi <patrick.lioi@gmail.com>
2022-03-08 12:37:04 -0600

    Install FluentValidation with integration into the MVC request pipeline.

--- src/ContactList.Server/Program.cs
+++ src/ContactList.Server/Program.cs
@@ -1,6 +1,11 @@
+using FluentValidation.AspNetCore;
 using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddControllersWithViews()
+    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Program>());
 
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddMediatR(typeof(Program));
