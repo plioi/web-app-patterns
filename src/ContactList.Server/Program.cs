ea05542c63f625b54169294da3aa5d7148336f57
Patrick Lioi <patrick.lioi@gmail.com>
2022-03-08 12:33:28 -0600

    Install MediatR.

--- src/ContactList.Server/Program.cs
+++ src/ContactList.Server/Program.cs
@@ -1,6 +1,9 @@
+using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddMediatR(typeof(Program));
 
 var app = builder.Build();
 
