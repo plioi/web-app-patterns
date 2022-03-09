0689adcbd80aa134f67c6ec0e11e4305013e56d5
Patrick Lioi <patrick.lioi@gmail.com>
2022-03-09 17:36:24 -0600

    Remove example content introduced by the "mvc" project template.

--- src/ContactList.Server/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ContactList.Server.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
