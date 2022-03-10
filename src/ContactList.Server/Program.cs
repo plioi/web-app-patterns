3db20f7ab21fc35b5504838f94f370a0466282ef
Patrick Lioi <patrick.lioi@gmail.com>
2022-03-10 11:46:39 -0600

    Remove example content introduced by the "blazorwasm" project template.

--- src/ContactList.Server/Program.cs
+++ src/ContactList.Server/Program.cs
@@ -2,14 +2,11 @@ using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -27,7 +24,6 @@ app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 
 app.MapRazorPages();
 app.MapControllers();
