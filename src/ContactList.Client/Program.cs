e4bdeebefbbbed97fbf457a27262a7fa1ed45513
Patrick Lioi <patrick.lioi@gmail.com>
2022-03-11 14:46:28 -0600

    Install and configure the Blazorise component library with its Bootstrap 5 integration.
       dotnet add src/ContactList.Client package Blazorise.Bootstrap5 --version 1.0.1
       dotnet add src/ContactList.Client package Blazorise.Icons.FontAwesome --version 1.0.1

--- src/ContactList.Client/Program.cs
+++ src/ContactList.Client/Program.cs
@@ -1,8 +1,17 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ContactList.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services
+    .AddBlazorise()
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
