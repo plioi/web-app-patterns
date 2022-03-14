a8f63f241d9ddf92b072449ed0068d370b01d048
Patrick Lioi <patrick.lioi@gmail.com>
2022-03-14 11:11:12 -0500

    Add Blazor front end using [ApiController] semantics.

--- src/ContactList.Contracts/AddContactCommand.cs
+++ src/ContactList.Contracts/AddContactCommand.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using MediatR;
 
 namespace ContactList.Contracts;
@@ -6,9 +5,6 @@ namespace ContactList.Contracts;
 public class AddContactCommand : IRequest<AddContactResponse>
 {
     public string? Email { get; set; }
-
     public string? Name { get; set; }
-
-    [Display(Name = "Phone Number")]
     public string? PhoneNumber { get; set; }
 }
